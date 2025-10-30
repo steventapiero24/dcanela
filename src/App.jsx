9bf9950685fd42a8bd176584e65775fa049f7246
steventapiero24 <steventapiero24@gmail.com>
2025-10-31 00:19:48 +0100

    basic estrucure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Catalogo from './components/Catalogo/Catalogo';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Marcas from './components/Marcas/Marcas';
+import Nav from './components/Nav/Nav';
+import Nosotros from './components/Nosotros/Nosotros';
 
 function App() {
 
   return (
     <>
-      <div>Hola mundo</div>
+      <Nav />
+      <Header />
+      <Nosotros />
+      <Catalogo />
+      <Marcas />  
+      <Footer />  
     </>
   )
 }
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='container__nav'>Nav</div>
+  )
+}
+
+export default Nav
