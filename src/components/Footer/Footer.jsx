511a0500186a32dac4e5ca1793d15dd45d26e5cd
steventapiero24 <steventapiero24@gmail.com>
2025-11-29 18:16:19 +0100

    nav gsap and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
+import './Footer.css'
+import Logo from '../../assets/images/cropped-Mi-publicación-2-3.png'
+import Logofacebook from '../../assets/images/facebook.png'
+import LogoIg from '../../assets/images/logotipo-de-instagram.png'
 
 const Footer = () => {
   return (
-    <div>
-      <h3>Encuentranos</h3>
-      <div>
+    <div className='container__footer'>
+      <div className='container__footer_contact'>
+        <h3>Encuentranos</h3>
         <h4>Dirección</h4>
         <p>Calle Mar del Norte, 1</p>
         <p>28821, Coslada (Madrid).</p>
       </div>
-      <div>
+      <div className='container__footer_logo'>
+       <img src={Logo} alt='Logo' />
+        iconos redes sociales
+        <div className='logos'>
+          <img src={Logofacebook} alt='Logo facebook' />
+          <img src={LogoIg} alt='Logo ig' />
+        </div>
+      </div>
+      <div className='container__footer_schedule'>
         <h4>Horario</h4>
         <p>Lunes a Viernes: <span> de 8:00 a 20:00 ininterrumpidamente. </span></p>
         <p>Sábados <span> de 8:00 a 14:30 ininterrumpidamente. </span></p>
         <p>Domingos <span> Cerrado </span></p>
-      </div>
-      <div>
-        iconos redes sociales
       </div>
     </div>
   )
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
 import Logo from '../../assets/images/cropped-Mi-publicación-2-3.png'
 import './Nav.css'
 
 const Nav = () => {
+
+  const navRef = useRef(null)
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+
+      gsap.from(".container__nav", {
+        y: -50,
+        opacity: 0,
+        filter: "blur(10px)",
+        duration: 1.4,
+        ease: "power3.out"
+      })
+
+      gsap.from(".container__nav-menu li", {
+        opacity: 0,
+        y: -20,
+        duration: 1,
+        stagger: 0.1,
+        delay: 0.4,
+        ease: "power3.out"
+      })
+
+      gsap.from(".container__nav-contant a", {
+        opacity: 0,
+        y: -20,
+        duration: 1,
+        stagger: 0.1,
+        delay: 0.6,
+        ease: "power3.out"
+      })
+
+      gsap.from(".container__nav-logo img", {
+        opacity: 0,
+        scale: 0.8,
+        duration: 1.2,
+        delay: 0.8,
+        ease: "power3.out"
+      })
+
+    }, navRef)
+
+    return () => ctx.revert()
+  }, [])
+
   return (
-    <div className='container__nav'>
+    <div ref={navRef} className='container__nav'>
       <div className='container__pad'>
           <div className='container__nav-menu'>
-              <li>
-              <a href='#'>Inicio</a>
-              </li>
-              <li>
-              <a href='#'>Catalogo</a>
-              </li>
-              <li>
-              <a href='#'>Marcas</a>
-              </li>
-              <li>
-              <a href='#'>Nosotros</a>  
-              </li>
+              <li><a href='#'>Inicio</a></li>
+              <li><a href='#'>Catalogo</a></li>
+              <li><a href='#'>Marcas</a></li>
+              <li><a href='#'>Nosotros</a></li>
           </div>
           <div className='container__nav-logo'>
               <img src={Logo} alt='Logo' />
